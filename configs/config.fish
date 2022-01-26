940d2035e3aede082743ffb0eb36495d41a2b9e6
elgamalsalman <elgamalsalman@gmail.com>
2022-01-27 00:01:07 +0200

    updated some configs

--- configs/config.fish
+++ configs/config.fish
@@ -1,8 +1,14 @@
 if status is-interactive
 	alias ls='exa -lags=type --color-scale --header --icons --git';
+	alias lss='exa -ags=type --color-scale --header --icons --git';
+	alias neofetch='echo;/usr/bin/neofetch';
 
 	neofetch;
 
-	set EXA_COLORS 'hd=30:uu=02;03;32:gu=02;03;32:';
+	set fish_greeting ""
+	set fish_color_cwd 1793d0
+	set fish_color_user 2773a0
+
+	set EXA_COLORS 'hd=30:uu=32:gu=32:sn=35:sb=35:da=03;34:uu=30:gu=30:ur=36:uw=36:ux=36:ue=36:gr=36:gw=36:gx=36:tr=36:tw=36:tx=36:';
 	export EXA_COLORS;
 end
